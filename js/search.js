fee39f52ff45036e77a270b962035970bc884584
Boris Budini <boris@budini.xyz>
2019-07-22 22:33:36 +0200

    Test2

--- js/search.js
+++ js/search.js
@@ -1,39 +1,3 @@
-const search = instantsearch({
-  appId: '{{ site.algolia.application_id }}',
-  apiKey: '{{ site.algolia.search_only_api_key }}',
-  indexName: '{{ site.algolia.index_name }}'
-});
-
-const hitTemplate = function(hit) {
-  let date = '';
-  if (hit.date) {
-    date = moment.unix(hit.date).format('MMM D, YYYY');
-  }
-
-  let url = `{{ site.baseurl }}${hit.url}`;
-
-  const title = hit._highlightResult.title.value;
-
-  let breadcrumbs = '';
-  if (hit._highlightResult.headings) {
-    breadcrumbs = hit._highlightResult.headings.map(match => {
-      return `<span class="post-breadcrumb">${match.value}</span>`
-    }).join(' > ')
-  }
-
-  const content = hit._highlightResult.html.value;
-
-  return `
-    <div class="post-item">
-      <h2><a class="post-link" href="${url}">${title}</a></h2>
-      {{#breadcrumbs}}<a href="${url}" class="post-breadcrumbs">${breadcrumbs}</a>{{/breadcrumbs}}
-      <div class="post-snippet">${content}</div>
-      <span class="post-meta">Written on ${date}</span>
-    </div>
-  `;
-}
-
-
 search.addWidget(
   instantsearch.widgets.searchBox({
     container: '#search-searchbar',
